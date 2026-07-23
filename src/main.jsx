e57c4b9b3468bbf638660e8070094508e0fc78b3
HyunoMo Koo <103720259+hmooko@users.noreply.github.com>
2026-07-23 12:57:47 +0900

    Activate editorial portfolio app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles.css';
-import './portfolio-enhancements.css';
-import App from './App.jsx';
+import './editorial.css';
+import EditorialApp from './EditorialApp.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <EditorialApp />
   </React.StrictMode>,
 );
