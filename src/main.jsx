06dbd19cb467b193b0617ad6f4abed0e24db6acd
HyunoMo Koo <103720259+hmooko@users.noreply.github.com>
2026-07-23 13:43:17 +0900

    Load liftoff animation fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './liftoff.css';
+import './liftoff-fixes.css';
 import LiftoffApp from './LiftoffApp.jsx';
 
 createRoot(document.getElementById('root')).render(
