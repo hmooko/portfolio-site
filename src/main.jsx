56960fc57d0abe7d47e427794590a1246b68c17a
HyunoMo Koo <103720259+hmooko@users.noreply.github.com>
2026-07-23 13:53:22 +0900

    Revert "Explore a cinematic product-launch portfolio direction"

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './liftoff.css';
-import './liftoff-fixes.css';
-import LiftoffApp from './LiftoffApp.jsx';
+import './editorial.css';
+import EditorialApp from './EditorialApp.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LiftoffApp />
+    <EditorialApp />
   </React.StrictMode>,
 );
