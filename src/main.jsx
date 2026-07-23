e0054483308e77db4d851ef17d9634131c61ddf7
HyunoMo Koo <103720259+hmooko@users.noreply.github.com>
2026-07-23 12:54:08 +0900

    Load Korean typography and readability refinements

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './experimental.css';
+import './experimental-ko.css';
 import './monochrome.css';
+import './readability.css';
 import ExperimentalApp from './ExperimentalApp.jsx';
 
 createRoot(document.getElementById('root')).render(
